28df6269735c172f2b6c4966e676c285e4b87806
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-05 08:28:00 -0600

    Created obejct that contains array of questiona nd anwsers, and divs to store those questions eventually on html

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var timeBtn = document.querySelector('#time');
 var landingTitle = document.querySelector('#first-page-title')
 var firstWelcome = document.querySelector('#first-page-welcome')
 var rulesDesc = document.querySelector('#rules')
-var timeScore = 100;
+var timeScore = 70;
 var Questions
 
 timeBtn.textContent = "Time: " + timeScore;
@@ -16,14 +16,98 @@ var startgame = function(event){
     firstWelcome.textContent = "";
     rulesDesc.textContent = "";
     startBtn.parentNode.remove();
+   
+    quizQuestions();
+}
 
-    
+var quizQuestions = function(event){
+ var questions = [
+  {
+      Question: "Which animal features in the logo for the Lamborghini?",
+      answers: {
+          a: "Horse",
+          b: "Jaguar",
+          c: "Bull",
+          d: "Girrafe"
+      },
+    correctAnswer = c
+    },
+    {
+        Question: "What was the original color for all Ferrari Models?",
+        answers: {
+            a: "Red",
+            b: "White",
+            c: "Black",
+            d: "Yellow"
+        },
+        correctAnswer = a
+    },
+    {
+        Question: "Which iconic car manufacturer also made airplane engines?",
+        answers: {
+            a: "Dodge",
+            b: "Rolls Royce",
+            c: "Mclaren",
+            d:  "Porshce"
+        },
+        correctAnswer = b 
+    },
+   {
+        Question: "What is the world's all-time best selling car?",
+        answers: {
+            a: "Toyota Camry",
+            b: "Honda Civic",
+            c: "Toyoa Corolla",
+            d:  "Ford F-150"
+        },
+        correctAnswer = c
+    },
+    {
+        Question: "The japanese car company Toyota was Originally established as a textiles manufacturer. True or false?",
+        answers: {
+            a: "False",
+            b: "True",
+        
+        },
+        correctAnswer = b 
+    },
+    {
+        Question: "What is generally considered to be the first pony car?",
+        answers: {
+            a: "Mecurry Cougar",
+            b: "Ford Mustang",
+            c: "Cheverolet Camaro",
+            d:  "Pontiac Firebird"
+        },
+        correctAnswer = b 
+    },
+    {
+        Question: "What was the first Japanese car to be produced in the United States?",
+        answers: {
+            a: "Nissan Maxima",
+            b: "Mazda Miata",
+            c: "Toyota Camry",
+            d: "Honda Accord"
+        },
+        correctAnswer = d 
+    },
+    {
+        Question: "What was the first car launched into space?",
+        answers: {
+            a: "Rinspeed Oasis",
+            b: "Porshce 911",
+            c: "Tesla Roadster",
+            d:  "Rolls Royce vision 100"
+        },
+        correctAnswer = b 
+    },
+
+
+ ]
 
 
 
 }
-
-
 
 
 
