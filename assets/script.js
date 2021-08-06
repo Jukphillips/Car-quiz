958bc450ee811c6679cf52760740eecc6cf714e0
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-05 20:34:31 -0600

    fixed bug that wouldn't let me use for loops on anwsers

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,22 @@
+//declares entire quiz container
+var quiz = document.querySelector('#quiz');
+
+
+
+// identifies Button for 
 var startBtn = document.querySelector("#start-quiz");
-var timeBtn = document.querySelector('#time');
+var timeBtn = document.querySelector('#time')
+var scoreboardButton = document.querySelector('#scoreboard-button') 
 var landingTitle = document.querySelector('#first-page-title')
 var firstWelcome = document.querySelector('#first-page-welcome')
 var rulesDesc = document.querySelector('#rules')
-var timeScore = 70;
-var Questions
 
-timeBtn.textContent = "Time: " + timeScore;
+var timescore = 70;
 
+
+
+
+timeBtn.textContent = "Time: " + timescore
 
 
 var startgame = function(event){
@@ -16,99 +25,72 @@ var startgame = function(event){
     firstWelcome.textContent = "";
     rulesDesc.textContent = "";
     startBtn.parentNode.remove();
-   
+    
     quizQuestions();
+
+    
+}
+
+
+var score = function(scoreevent) {
+    scoreevent.preventDefault();
+    landingTitle.textContent = "";
+    firstWelcome.textContent = "";
+    rulesDesc.textContent = "";
+    startBtn.parentNode.remove();
+
+}
+var leaderboards = function(scoreevent) {
+
 }
 
 var quizQuestions = function(event){
- var questions = [
-  {
-      Question: "Which animal features in the logo for the Lamborghini?",
-      answers: {
-          a: "Horse",
-          b: "Jaguar",
-          c: "Bull",
-          d: "Girrafe"
-      },
-    correctAnswer = c
-    },
-    {
+    var questions = [
+ {
+     Question: "Which animal features in the logo for the Lamborghini?",
+      answers: ["1.Horse","2.Jaguar","3.Bull", "4.Girrafe" ],
+     correctAnswer: "Bull"
+    }, {
         Question: "What was the original color for all Ferrari Models?",
-        answers: {
-            a: "Red",
-            b: "White",
-            c: "Black",
-            d: "Yellow"
-        },
-        correctAnswer = a
-    },
-    {
+        answers: ["Red", "White","Black", "Yellow" ],
+        correctAnswer: "Red"
+    }, {
         Question: "Which iconic car manufacturer also made airplane engines?",
-        answers: {
-            a: "Dodge",
-            b: "Rolls Royce",
-            c: "Mclaren",
-            d:  "Porshce"
-        },
-        correctAnswer = b 
-    },
-   {
+        answers: ["Dodge","Rolls Royce", "Mclaren", "Porshce"],
+        correctAnswer: "Rolls Royce"
+    }, {
         Question: "What is the world's all-time best selling car?",
-        answers: {
-            a: "Toyota Camry",
-            b: "Honda Civic",
-            c: "Toyoa Corolla",
-            d:  "Ford F-150"
-        },
-        correctAnswer = c
-    },
-    {
-        Question: "The japanese car company Toyota was Originally established as a textiles manufacturer. True or false?",
-        answers: {
-            a: "False",
-            b: "True",
-        
-        },
-        correctAnswer = b 
-    },
-    {
+        answers: ["Toyota Camry", "Honda Civic", "Toyoa Corolla", "Ford F-150" ],
+        correctAnswer: "Toyota Corolla"
+    }, {
+        Question: "How much horse power did the first Porsche 911 have?",
+        answers: ["90 HP", "130 HP", "35 HP", "190 HP"],
+        correctAnswer: "130" 
+    }, {
         Question: "What is generally considered to be the first pony car?",
-        answers: {
-            a: "Mecurry Cougar",
-            b: "Ford Mustang",
-            c: "Cheverolet Camaro",
-            d:  "Pontiac Firebird"
-        },
-        correctAnswer = b 
-    },
-    {
+        answers: ["Mecurry Cougar", "Ford Mustang", "Cheverolet Camaro", "Pontiac Firebird"],
+        correctAnswer: "Ford Mustang"
+    }, {
         Question: "What was the first Japanese car to be produced in the United States?",
-        answers: {
-            a: "Nissan Maxima",
-            b: "Mazda Miata",
-            c: "Toyota Camry",
-            d: "Honda Accord"
-        },
-        correctAnswer = d 
-    },
-    {
+        answers: {a: "Nissan Maxima", b: "Mazda Miata", c: "Toyota Camry", d: "Honda Accord"},
+        correctAnswer: 'Honda Accord' 
+    }, {
         Question: "What was the first car launched into space?",
-        answers: {
-            a: "Rinspeed Oasis",
-            b: "Porshce 911",
-            c: "Tesla Roadster",
-            d:  "Rolls Royce vision 100"
-        },
-        correctAnswer = b 
-    },
-
-
- ]
-
-
+        answers: {a: "Rinspeed Oasis", b: "Porshce 911", c: "Tesla Roadster", d:  "Rolls Royce vision 100"},
+        correctAnswer: "Tesla Roadster" 
+    }, ]
+    console.log(questions[1].Question)
+    console.log(questions[1].answers.length)
+    
+    for( var i= 0; i < questions.length; ) {
+        
+    }
 
 }
 
 
 
+
+
+scoreboardButton.addEventListener('click', score)
 startBtn.addEventListener('click', startgame); 
