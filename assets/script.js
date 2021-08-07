c5c143d7e3303b962732de657d96e460c34bef63
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-07 01:03:42 -0600

    Figured out way to connect buttons to js we can develop logcial around them

--- assets/script.js
+++ assets/script.js
@@ -25,11 +25,17 @@ var anw3 = document.querySelector('#anwBtn-3')
 var anw4 = document.querySelector('#anwBtn-4')
 // declares button for the scoreboard
 var scoreboardButton = document.querySelector('#scoreboard-button') 
+//
+var ButtonCss = document.querySelector('.buttonCss')
+
+
 
 // var declaring the inital value of time
 var timescore = 70;
 // var declaring question count
 var questionCount = 0;
+// var declaring i in global scope
+var i = 0;
 
 
 // declares our questions, anwser choices, and anwsers
@@ -80,14 +86,15 @@ function startgame (event) {
     landingTitle.textContent = "";
     firstWelcome.textContent = "";
     rulesDesc.textContent = "";
-    startBtn.parentNode.remove();  
+    startBtn.parentNode.remove(); 
+    // timer(); 
     quizQuestions();
 
     
 };
 
 
-function score(scoreevent) {
+function score() {
 
     landingTitle.textContent = "";
     firstWelcome.textContent = "";
@@ -95,27 +102,40 @@ function score(scoreevent) {
     startBtn.parentNode.remove();
 
 }
-var leaderboards = function(scoreevent) {
+var leaderboards = function() {
 
 }
 
 var test = document.getElementById("test")
 function quizQuestions(){
     quiz.style.display = "block";
-    var i = 0;
+    
     if (questionCount < questions.length ) {
         questionPlace.textContent = questions[i].Question;
-        anw1.innerHTML = questions[i].answers[0];
+        anw1.textContent = questions[i].answers[0];
         anw2.textContent = questions[i].answers[1];
         anw3.textContent = questions[i].answers[2];
         anw4.textContent = questions[i].answers[3];
+    }
+}
+    
+    function checkAnwser(event) {
 
 
+      
+
+
+        if(event === questions[i].correctAnswer  ) {
+            alert('you did it')
+        } else if (event === questions[i].correctAnswer) {
+            alert('you did it')
+    } else if (event === questions[i].correctAnswer) {
+            alert('you did it')
+    } else if (event === questions[i].correctAnswer) {
+            alert('you did it')
     }
 
-
-
-}
+    }
 
 console.log(questions[1].Question)
 
@@ -123,3 +143,21 @@ console.log(questions[1].Question)
 
 scoreboardButton.addEventListener('click', score)
 startBtn.addEventListener('click', startgame); 
+
+
+anw1.addEventListener("click", function (event) {
+  var event = event.target;
+  checkAnwser(event.textContent.trim());
+});
+anw2.addEventListener("click", function (event) {
+  var event = event.target;
+  checkAnwser(event.textContent.trim());
+});
+anw3.addEventListener("click", function (event) {
+  var event = event.target;
+  checkAnwser(event.textContent.trim());
+});
+anw4.addEventListener("click", function (event) {
+  var event = event.target;
+  checkAnwser(event.textContent.trim());
+});
